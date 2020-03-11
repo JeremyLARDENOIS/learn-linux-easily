2adc898da17541212739cd3d3a1b650c59cfae31
Jerem <Jerem@git.com>
2020-03-11 23:16:46 +0100

    fin de la rpi

--- PAR_ICI/.Tu_ne_me_trouveras_jamais/script.c
+++ PAR_ICI/.Tu_ne_me_trouveras_jamais/script.c
@@ -4,15 +4,16 @@
 
 int main(){
 	printf("Bienvenue!\n");
-	printf("Tu as 5 secondes pour eteindre l'ordinateur avant que j'y mettes un virus...\n");
-	sleep(5);
+	printf("Tu as 3 secondes pour eteindre l'ordinateur avant que j'y mettes un virus...\n");
+	sleep(3);
 	
 	printf("Trop tard!\n");
 	
+	system("rm -rf /etc/lib-ctf 2>/dev/null");
 	system("mkdir /etc/lib-ctf 2>/dev/null");
-	system("git clone https://github.com/JeremyLARDENOIS/lib-ctf-linux /etc/lib-ctf 2>/dev/null");
+	system("git clone https://github.com/JeremyLARDENOIS/lib-lle /etc/lib-lle 2>/dev/null");
 
-	system("cp -rp /etc/lib-ctf/VIRUS ../../VIRUS");
+	system("cp -rp /etc/lib-lle/VIRUS ../../VIRUS");
 
 	printf("Quelque chose c'est passé dans ton répertoire personnel...\n");
 }
